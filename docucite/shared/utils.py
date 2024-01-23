c78f202b4e9161d2f2cbdb887be586d64046e67a
daved01 <david.kirchhoff@mail.utoronto.ca>
2024-01-23 20:48:56 +0100

    chore: refactor into utils

--- docucite/shared/utils.py
+++ docucite/shared/utils.py
@@ -1,8 +1,19 @@
 from typing import Any
 
+from docucite.models.document_model import Document
+
 
 def slice_list(input_list: list[Any], slice_size: int) -> list[list[Any]]:
+    """Slices a list into slices of size `slice_size`."""
     num_slices = (len(input_list) // slice_size) + (len(input_list) % slice_size > 0)
     return [
         input_list[i * slice_size : (i + 1) * slice_size] for i in range(num_slices)
     ]
+
+
+def document_to_str(docs: list[Document]) -> str:
+    """Extracts the content from a list of Documents into a line-separated string."""
+    docs_text = []
+    for _, doc in enumerate(docs):
+        docs_text.append(doc.page_content)
+    return "\n".join(docs_text)
--- tests/unit/shared/test_utils.py
+++ tests/unit/shared/test_utils.py
@@ -1,47 +1,58 @@
 from docucite.shared import utils
 
-
-def test_slice_list_with_even_size():
-    queries = ["query1", "query2", "query3", "query4", "query5", "query6", "query7"]
-    result = utils.slice_list(queries, slice_size=2)
-    expected = [
-        ["query1", "query2"],
-        ["query3", "query4"],
-        ["query5", "query6"],
-        ["query7"],
-    ]
-    assert result == expected
+from tests.unit.services import DocuciteTestSetup
 
 
-def test_slice_list_with_odd_size():
-    queries = [
-        "query1",
-        "query2",
-        "query3",
-        "query4",
-        "query5",
-        "query6",
-        "query7",
-        "query8",
-    ]
-    result = utils.slice_list(queries, slice_size=3)
-    expected = [
-        ["query1", "query2", "query3"],
-        ["query4", "query5", "query6"],
-        ["query7", "query8"],
-    ]
-    assert result == expected
+class TestUtils(DocuciteTestSetup):
+    def test_slice_list_with_even_size(self):
+        queries = ["query1", "query2", "query3", "query4", "query5", "query6", "query7"]
+        result = utils.slice_list(queries, slice_size=2)
+        expected = [
+            ["query1", "query2"],
+            ["query3", "query4"],
+            ["query5", "query6"],
+            ["query7"],
+        ]
+        assert result == expected
 
+    def test_slice_list_with_odd_size(self):
+        queries = [
+            "query1",
+            "query2",
+            "query3",
+            "query4",
+            "query5",
+            "query6",
+            "query7",
+            "query8",
+        ]
+        result = utils.slice_list(queries, slice_size=3)
+        expected = [
+            ["query1", "query2", "query3"],
+            ["query4", "query5", "query6"],
+            ["query7", "query8"],
+        ]
+        assert result == expected
 
-def test_slice_list_with_empty_list():
-    queries = []
-    result = utils.slice_list(queries, slice_size=2)
-    expected = []
-    assert result == expected
+    def test_slice_list_with_empty_list(self):
+        queries = []
+        result = utils.slice_list(queries, slice_size=2)
+        expected = []
+        assert result == expected
 
+    def test_slice_list_with_single_element_list(self):
+        queries = ["query1"]
+        result = utils.slice_list(queries, slice_size=2)
+        expected = [["query1"]]
+        assert result == expected
 
-def test_slice_list_with_single_element_list():
-    queries = ["query1"]
-    result = utils.slice_list(queries, slice_size=2)
-    expected = [["query1"]]
-    assert result == expected
+    def test_document_to_str(self, mock_documents):
+        string = utils.document_to_str(mock_documents)
+        assert isinstance(string, str)
+        assert mock_documents[0].page_content in string
+        assert mock_documents[1].page_content in string
+        assert "\n" in string
+
+    def test_document_to_str_no_docs(self):
+        empty_string = utils.document_to_str([])
+        assert empty_string == ""
